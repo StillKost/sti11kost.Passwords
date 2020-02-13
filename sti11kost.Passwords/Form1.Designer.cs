3b141b70daef53a2e02e5ba2149395caa521108f
StillKost <45827311+StillKost@users.noreply.github.com>
2020-02-14 02:11:55 +0300

    изменил иконку приложения

--- sti11kost.Passwords/Form1.Designer.cs
+++ sti11kost.Passwords/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.size = new System.Windows.Forms.TextBox();
             this.sizeError = new System.Windows.Forms.Label();
@@ -252,6 +253,7 @@
             this.Controls.Add(this.size);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
